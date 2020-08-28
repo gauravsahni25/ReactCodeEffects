5cf9a2b5c1518458a4211c13e791523d568f1f30
gauravs <gauravs@blinds.com>
2020-08-28 13:01:10 -0500

    Added Classes

--- ReactEffects.Api/Controllers/RulesController.cs
+++ ReactEffects.Api/Controllers/RulesController.cs
@@ -1,175 +1,177 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using CodeEffects.Rule.Common;
-//using CodeEffects.Rule.Core;
-//using CodeEffects.Rule.Web;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodeEffects.Rule.Common;
+using CodeEffects.Rule.Core;
+using CodeEffects.Rule.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using ReactEffects.Api.Models;
+using ReactEffects.Api.Services;
 
-//namespace ReactEffects.Api.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class RulesController : ControllerBase
-//    {
-//		private readonly IWebHostEnvironment environment;
+namespace ReactEffects.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RuleController : ControllerBase
+    {
+        private readonly IWebHostEnvironment environment;
 
-//		public RuleController(IConfiguration config, IWebHostEnvironment environment)
-//		{
-//			this.environment = environment;
-//		}
+        public RuleController(IConfiguration config, IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
 
-//		// Returns an initial 
-//		[HttpGet("settings/{editorId}")]
-//		public ClientSettings GetSettings(string editorId)
-//		{
-//			RuleEditor editor = GetRuleEditor(editorId);
+        // Returns an initial 
+        [HttpGet("settings/{editorId}")]
+        public ClientSettings GetSettings(string editorId)
+        {
+            RuleEditor editor = GetRuleEditor(editorId);
 
-//			ClientSettings settings = new ClientSettings
-//			{
-//				EditorData = editor.GetInitialSettings(),
-//				SourceData = editor.GetClientSettings(),
-//			};
+            ClientSettings settings = new ClientSettings
+            {
+                EditorData = editor.GetInitialSettings(),
+                SourceData = editor.GetClientSettings(),
+            };
 
-//			return settings;
-//		}
+            return settings;
+        }
 
-//		// Loads an existing rule
-//		// GET api/<RuleController>/divRuleEditor/5
-//		[HttpGet("{editorId}/{id}")]
-//		public IActionResult Get(string editorId, Guid id)
-//		{
-//			string ruleXml = new StorageService(environment).LoadRuleXml(id.ToString());
+        // Loads an existing rule
+        // GET api/<RuleController>/divRuleEditor/5
+        [HttpGet("{editorId}/{id}")]
+        public IActionResult Get(string editorId, Guid id)
+        {
+            string ruleXml = new StorageService(environment).LoadRuleXml(id.ToString());
 
-//			RuleEditor editor = GetRuleEditor(editorId);
-//			editor.LoadRuleXml(ruleXml);
+            RuleEditor editor = GetRuleEditor(editorId);
+            editor.LoadRuleXml(ruleXml);
 
-//			string ruleJson = editor.GetClientRuleData();
+            string ruleJson = editor.GetClientRuleData();
 
-//			// Send it back to the server
-//			return new JsonResult(ruleJson);
-//		}
+            // Send it back to the server
+            return new JsonResult(ruleJson);
+        }
 
-//		// Saves the rule
-//		// POST api/<RuleController>
-//		[HttpPost("{editorId}")]
-//		public Result Post([FromBody] string value, string editorId)
-//		{
-//			Result result = new Result();
+        // Saves the rule
+        // POST api/<RuleController>
+        [HttpPost("{editorId}")]
+        public Result Post([FromBody] string value, string editorId)
+        {
+            Result result = new Result();
 
-//			RuleEditor editor = GetRuleEditor(editorId);
-//			editor.LoadClientData(value);
+            RuleEditor editor = GetRuleEditor(editorId);
+            editor.LoadClientData(value);
 
-//			if (editor.Rule.IsEmpty())
-//			{
-//				result.IsRuleEmpty = true;
-//			}
-//			else if (!editor.Rule.IsValid(new StorageService(environment).LoadRuleXml))
-//			{
-//				result.IsRuleValid = false;
-//				result.ClientInvalidData = editor.GetClientInvalidData();
-//			}
-//			else
-//			{
-//				new StorageService(environment).SaveRule(
-//					editor.Rule.Id.ToString(),
-//					editor.Rule.GetRuleXml(),
-//					editor.Rule.IsLoadedRuleOfEvalType == null || (bool)editor.Rule.IsLoadedRuleOfEvalType
-//				);
+            if (editor.Rule.IsEmpty())
+            {
+                result.IsRuleEmpty = true;
+            }
+            else if (!editor.Rule.IsValid(new StorageService(environment).LoadRuleXml))
+            {
+                result.IsRuleValid = false;
+                result.ClientInvalidData = editor.GetClientInvalidData();
+            }
+            else
+            {
+                new StorageService(environment).SaveRule(
+                    editor.Rule.Id.ToString(),
+                    editor.Rule.GetRuleXml(),
+                    editor.Rule.IsLoadedRuleOfEvalType == null || (bool)editor.Rule.IsLoadedRuleOfEvalType
+                );
 
-//				result.Output = editor.Rule.Id.ToString();
-//			}
+                result.Output = editor.Rule.Id.ToString();
+            }
 
-//			return result;
-//		}
+            return result;
+        }
 
-//		// Deletes an existing rule
-//		// DELETE api/<RuleController>/5
-//		[HttpDelete("{id}")]
-//		public JsonResult Delete(Guid id)
-//		{
-//			// Delete the rule from the storage file by its ID
-//			new StorageService(environment).DeleteRule(id.ToString());
+        // Deletes an existing rule
+        // DELETE api/<RuleController>/5
+        [HttpDelete("{id}")]
+        public JsonResult Delete(Guid id)
+        {
+            // Delete the rule from the storage file by its ID
+            new StorageService(environment).DeleteRule(id.ToString());
 
-//			return new JsonResult(null);
-//		}
+            return new JsonResult(null);
+        }
 
-//		// Evaluates a rule against the data from test form
-//		[HttpPost("evaluate/{editorId}")]
-//		public JsonResult Evaluate(string editorId, [FromBody] EvaluateRequest evaluateRequest)
-//		{
-//			EvaluateResponse response = new EvaluateResponse();
+        // Evaluates a rule against the data from test form
+        [HttpPost("evaluate/{editorId}")]
+        public JsonResult Evaluate(string editorId, [FromBody] EvaluateRequest evaluateRequest)
+        {
+            EvaluateResponse response = new EvaluateResponse();
 
-//			// Get an instance of the Editor
-//			RuleEditor editor = GetRuleEditor(editorId);
+            // Get an instance of the Editor
+            RuleEditor editor = GetRuleEditor(editorId);
 
-//			// Tell the editor not to enforce the rule name requirement -
-//			// we don't save the rule, just evaluate it, so we don't care about its name
-//			editor.RuleNameIsRequired = false;
+            // Tell the editor not to enforce the rule name requirement -
+            // we don't save the rule, just evaluate it, so we don't care about its name
+            editor.RuleNameIsRequired = false;
 
-//			// Load the rule data from the client
-//			editor.LoadClientData(evaluateRequest.RuleData);
+            // Load the rule data from the client
+            editor.LoadClientData(evaluateRequest.RuleData);
 
-//			if (editor.IsEmpty)
-//			{
-//				response.IsRuleEmpty = true;
-//			}
-//			else if (!editor.IsValid)
-//			{
-//				// The rule is invalid. Load the invalid data and pass it to the client
-//				response.IsRuleValid = false;
-//				response.ClientInvalidData = editor.GetClientInvalidData();
-//			}
-//			else
-//			{
-//				EvaluationParameters p =
-//					new EvaluationParameters
-//					{
-//						Precision = 1,
-//						MidpointRounding = MidpointRounding.AwayFromZero,
-//						RuleGetter = new StorageService(this.environment).LoadRuleXml
-//					};
+            if (editor.IsEmpty)
+            {
+                response.IsRuleEmpty = true;
+            }
+            else if (!editor.IsValid)
+            {
+                // The rule is invalid. Load the invalid data and pass it to the client
+                response.IsRuleValid = false;
+                response.ClientInvalidData = editor.GetClientInvalidData();
+            }
+            else
+            {
+                EvaluationParameters p =
+                    new EvaluationParameters
+                    {
+                        Precision = 1,
+                        MidpointRounding = MidpointRounding.AwayFromZero,
+                        RuleGetter = new StorageService(this.environment).LoadRuleXml
+                    };
 
-//				// The rule is valid. Evaluate it.
-//				// The parameter Storage.LoadRuleXml is a delegate that evaluator
-//				// needs for looking up reusable rules that this rule might reference.
-//				Evaluator evaluator = new Evaluator(evaluateRequest.Patient.GetType(), editor.GetRuleXml(), p);
-//				bool success = evaluator.Evaluate(evaluateRequest.Patient);
+                // The rule is valid. Evaluate it.
+                // The parameter Storage.LoadRuleXml is a delegate that evaluator
+                // needs for looking up reusable rules that this rule might reference.
+                Evaluator evaluator = new Evaluator(evaluateRequest.Patient.GetType(), editor.GetRuleXml(), p);
+                bool success = evaluator.Evaluate(evaluateRequest.Patient);
 
-//				// Sending back the evaluated instance of the Patient
-//				response.Patient = evaluateRequest.Patient;
+                // Sending back the evaluated instance of the Patient
+                response.Patient = evaluateRequest.Patient;
 
-//				response.Output =
-//					string.IsNullOrWhiteSpace(evaluateRequest.Patient.Output) ?
-//					"The rule evaluated to " + success.ToString() :
-//					evaluateRequest.Patient.Output;
-//			}
+                response.Output =
+                    string.IsNullOrWhiteSpace(evaluateRequest.Patient.Output) ?
+                    "The rule evaluated to " + success.ToString() :
+                    evaluateRequest.Patient.Output;
+            }
 
-//			return new JsonResult(response);
-//		}
+            return new JsonResult(response);
+        }
 
-//		private RuleEditor GetRuleEditor(string editorId)
-//		{
-//			Type type = typeof(Models.Patient);
+        private RuleEditor GetRuleEditor(string editorId)
+        {
+            Type type = typeof(Models.Patient);
 
-//			RuleEditor editor = new RuleEditor(editorId)
-//			{
-//				Mode = RuleType.Execution,
-//				SourceType = type
-//			};
+            RuleEditor editor = new RuleEditor(editorId)
+            {
+                Mode = RuleType.Execution,
+                SourceType = type
+            };
 
-//			StorageService storage = new StorageService(environment);
+            StorageService storage = new StorageService(environment);
 
-//			// These are the rules displayed in the Rules menu of the ToolBar
-//			editor.ToolBarRules = storage.GetAllRules();
-//			// These are reusable evaluation rules displayed in context menus of the Rule Area; see documentation details on reusable rules
-//			editor.ContextMenuRules = storage.GetEvaluationRules();
+            // These are the rules displayed in the Rules menu of the ToolBar
+            editor.ToolBarRules = storage.GetAllRules();
+            // These are reusable evaluation rules displayed in context menus of the Rule Area; see documentation details on reusable rules
+            editor.ContextMenuRules = storage.GetEvaluationRules();
 
-//			return editor;
-//		}
-//	}
-//}
+            return editor;
+        }
+    }
+}
--- ReactEffects.Api/Models/Class.cs
+++ ReactEffects.Api/Models/Class.cs
@@ -2,10 +2,139 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodeEffects.Rule.Attributes;
+using CodeEffects.Rule.Common;
+using ReactEffects.Api.Services;
 
 namespace ReactEffects.Api.Models
 {
-    public class Class
+    public class ClientSettings
     {
+        public string EditorData { get; set; }
+        public string SourceData { get; set; }
+
+        public ClientSettings() { }
+    }
+
+	public class EvaluateRequest
+	{
+		public Patient Patient { get; set; }
+		public string RuleData { get; set; }
+
+		/*		public EvaluateRequest()
+				{
+				}
+		*/
+	}
+
+	/*	public class PatientX
+		{
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public GenderX Gender { get; set; }
+			public bool Alcohol { get; set; }
+			public int Pulse { get; set; }
+
+			public PatientX() { }
+		}
+
+		public enum GenderX
+		{
+			Male,
+			Female,
+			Undefined
+		}*/
+
+    public class EvaluateResponse : Result
+    {
+        public Patient Patient { get; set; }
+
+        public EvaluateResponse() : base() { }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        [ExcludeFromEvaluation]
+        Unknown
+    }
+
+	// External methods and actions
+	[ExternalAction(typeof(PatientService), "RequestInfo")]
+	public class Patient
+	{
+		// C-tor
+		public Patient()
+		{
+			this.ID = Guid.Empty;
+			this.Gender = Gender.Unknown;
+			this.Alcohol = false;
+		}
+
+		// This property will not appear in the Rule Editor - Code Effects component ignores Guids
+		// Details at http://codeeffects.com/Doc/Business-Rules-Data-Types
+		public Guid ID { get; set; }
+
+		[Field(DisplayName = "First Name", Description = "Patient's first name", ValueInputType = ValueInputType.User, Max = 30)]
+		public string FirstName { get; set; }
+
+		[Field(DisplayName = "Last Name", Max = 30, ValueInputType = ValueInputType.User, Description = "Patient's last name")]
+		public string LastName { get; set; }
+
+		[Field(ValueInputType = ValueInputType.User, Description = "Patient's gender")]
+		public Gender Gender { get; set; }
+
+		[Field(Min = 0, Max = 200, Description = "Current pulse")]
+		public int? Pulse { get; set; }
+
+		[Field(DisplayName = "Alcohol Box", Description = "Alcohol use?")]
+		public bool Alcohol { get; set; }
+
+		// This property is used to display the output of rule actions
+		[ExcludeFromEvaluation]
+		public string Output { get; set; }
+
+		[Method("Full Name", "Joins together patient's first and last names")]
+		public string FullName()
+		{
+			return string.Format("{0} {1}", this.FirstName, this.LastName);
+		}
+
+		// Empty overload of the Register method.
+		[Action(Description = " Registers new patient")]
+		public void Register()
+		{
+			this.Output += " The patient has been registered";
+		}
+
+		// This is the overload of the Register method that takes one param.
+		// Both overloads can be used in Code Effects as two different actions
+		// as long as their display names are different.
+		[Action("Register with a Message", "Registers new patient with additional info")]
+		public void Register([Parameter(ValueInputType.User, Description = "Output message")] string message)
+		{
+			this.Output += " " + message;
+		}
+	}
+
+    public class Result
+    {
+        public bool IsRuleEmpty { get; set; }
+        public bool IsRuleValid { get; set; }
+        public string Output { get; set; }
+        public string ClientInvalidData { get; set; }
+
+        public Result()
+        {
+            IsRuleEmpty = false;
+            IsRuleValid = true;
+        }
+    }
+
+    public class Rule
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
